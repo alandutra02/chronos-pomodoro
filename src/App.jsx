860aca52fcda0f301662d9fb1f915327046d23b0
“jorge” <alandutra02@yahoo.com.br>
2025-07-20 03:10:42 -0300

    style css DefaultInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Container } from './components/Container'
 import { Logo } from './components/Logo'
 import { Menu } from './components/Menu'
+import { CountDown } from './components/CountDown'
+import { DefaultInput } from './components/DefaultInput'
 
 import './styles/theme.css'
 import './styles/global.css'
@@ -11,9 +13,42 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
       </Container>
+
+      <Container>
+        <CountDown/>
+      </Container>
+
+      <Container>
+        <form className='form' action="">
+          <div className="formRow">
+            <DefaultInput
+              id='meuInput'
+              type='text'
+              placeholder='Digite a tarefa aqui'>
+                task
+              </DefaultInput>
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+
+        </form>
+      </Container>
+
     </>
   )    
 }
---
+++ src/components/DefaultInput/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.css'
+
+type propsDefaultInput = {
+    id: string;
+    children: React.ReactNode
+} & React.ComponentProps<'input'>; // neste caso estamos usando as propriedades padrão do componente type juntamente com o que criamos do tipo text, number e search. Isso é possível com o React.ComponentProps<>. O React.ComponentProps<> não é possível ser usado com interface. 
+
+export function DefaultInput({ children, type, id, ...rest }: propsDefaultInput) {
+    return (
+        <>
+            <label htmlFor={id}>{children}</label>
+            <input className={styles.input} id={id} type={type} {...rest}/>
+        </>
+    )
+}
