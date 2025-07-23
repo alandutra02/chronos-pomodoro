7594057b849502bb1b5ac98b14562cf8a469c31e
alandutra02 <alandutra02@yahoo.com.br>
2025-07-23 16:27:48 -0300

    useeffect

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,6 @@
 import { HouseIcon, HistoryIcon, SettingsIcon, SunIcon } from 'lucide-react'
 import styles from './styles.module.css'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 type AvailableThemes = 'dark' | 'light'
 
@@ -11,15 +11,28 @@ export function Menu() {
     event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
   ) {
     event.preventDefault()
-    console.log('Clicado', Date.now())
     setTheme(prevTheme => {
       const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
       return nextTheme
     })
-    document.documentElement.setAttribute('data-theme', theme)
   }
-
   
+    /* useEffect(() => {
+    console.log('useEffect sem dependências', Date.now())
+    }) // executado toda vez que o componente renderiza na tela 
+    
+       useEffect(() => {
+      console.log('useEffect com array de dependências vazio', Date.now())
+      },[]) // Executa apenas quando o React monta o componente na tela pela primeira vez */
+      
+      useEffect(() => {
+        console.log('Theme mudou', theme, Date.now())
+        document.documentElement.setAttribute('data-theme', theme)
+        return () => { // o conteudo de retun será colocado em um gatilho para ser renderizado apenas na próxima execução do evento ou componente
+          console.log('Olha, este componente será atualizado')
+        }
+  },[theme]) // Executa apenas quando o valor de theme muda
+
   return (
     <nav className={styles.menu}>
       <h1>{theme}</h1>
