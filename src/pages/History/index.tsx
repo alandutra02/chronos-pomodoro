2ad823b5d15247d2357c906e8e2574a68a252550
“jorge” <alandutra02@yahoo.com.br>
2025-09-06 20:06:58 -0300

    final

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -45,14 +45,13 @@ export function History() {
 
         setConfirmClearHistory(false)
 
-        //dispatch({ type: TaskActionTypes.RESET_STATE })
     }, [confirmClearHistory, dispatch])
 
     useEffect(() => {
         return () => {
             showMessage.dismiss()
         }
-    })
+    }, [])
 
     function handleSortTasks({field}: Pick<SortTasksOptions, 'field'>) {
         const newDirection = sortTasksOptions.direction === 'desc' ? 'asc' : 'desc'
